2b5d5c7f99249e1fd775aaeec39c72e69668c310
Obyat Abdelrahman <rahman.obyat@gmail.com>
2021-01-30 22:59:55 -0500

    created mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import { connect } from 'mongoose';
+
 
 const app = express();
 const PORT = 4000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+
+mongoose.set();
+mongoose.set();
+mongoose.connect('mongodb://localhost/productsdb', 
+{
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,3 +29,4 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
